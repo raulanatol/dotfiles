e0bff10c910cb11ef6cbdd7ceb61c6525758d6b3
raulanatol <raul@natol.es>
2021-01-17 11:16:55 +0000

    Test watch and zoom open

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,7 @@
 hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "E", function()
     hs.execute("dot mac browser_goto", true)
 end)
+
+hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "Z", function()
+    hs.execute("dot mac zoom start", true)
+end)
--- zsh/aliases/dev.sh
+++ zsh/aliases/dev.sh
@@ -1,3 +1,4 @@
 alias devs="./.scripts/devs.sh"
 alias check="make"
 alias finish-release="./.scripts/ci/finish-release"
+alias tw="npm run test -- --watch"
