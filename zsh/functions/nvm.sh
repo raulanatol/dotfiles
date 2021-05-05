0ee5b01dabbf3469e2256ced886c21704a2b79aa
raulanatol <raul@natol.es>
2021-05-05 12:04:58 +0100

    Cleanup nvm

--- zsh/functions/nvm.sh
+++ zsh/functions/nvm.sh
@@ -1,20 +1,20 @@
-lazynvm() {
-  unset -f nvm node npm npx
-  export NVM_DIR=~/.nvm
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-  if [ -f "$NVM_DIR/bash_completion" ]; then
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-  fi
-}
+#lazynvm() {
+#  unset -f nvm node npm npx
+#  export NVM_DIR=~/.nvm
+#  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+#  if [ -f "$NVM_DIR/bash_completion" ]; then
+#    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+#  fi
+#}
 
 lazyfnm() {
-  unset -f fnm node npm npx
+  unset -f fnm node npm npx nvm
   [ ! -s "$FNM_DIR" ] && eval "$(fnm env)"
 }
 
 nvm() {
-  lazynvm
-  nvm $@
+  lazyfnm
+  fnm $@
 }
 
 node() {
