e5b68208b27a95d6851da8fda6403a003b6007f1
yodralopez <yodralopez@gmail.com>
2021-05-26 09:45:38 +0100

    Checkpoint 26_05_2021

--- mac/install.sh
+++ mac/install.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-## Inspired by https://github.com/mathiasbynens/dotfiles/blob/master/.macos
-#
-## Close any open System Preferences panes, to prevent them from overriding
-## settings we’re about to change
-#osascript -e 'tell application "System Preferences" to quit'
-#
-#sudo -v
-#
-## Keep-alive: update existing `sudo` time stamp until `.macos` has finished
-#while true; do
-#  sudo -n true
-#  sleep 60
-#  kill -0 "$$" || exit
-#done 2>/dev/null &
-#
-## Disable the sound effects on boot
-#sudo nvram SystemAudioVolume=" "
-#
-## Play user interface sound effects: false
+# Inspired by https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Play user interface sound effects: false
 defaults write com.apple.systemsound "com.apple.sound.uiaudio.enabled" -int 0
 
 # Disable Big Sur Chime
@@ -78,64 +78,64 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 ################################################################################
 ## Dock, Dashboard, and hot corners                                            #
 ################################################################################
-#
-## Enable highlight hover effect for the grid view of a stack (Dock)
-#defaults write com.apple.dock mouse-over-hilite-stack -bool true
-#
-## Dock icons size
-#defaults write com.apple.dock tilesize -int 56
-#
-## Change minimize/maximize window effect
-#defaults write com.apple.dock mineffect -string "genie"
-#
-## Minimize windows into their application’s icon
-#defaults write com.apple.dock minimize-to-application -bool false
-#
-## Show indicator lights for open applications in the Dock
-#defaults write com.apple.dock show-process-indicators -bool true
-#
-## Don’t animate opening applications from the Dock
-#defaults write com.apple.dock launchanim -bool false
-#
-## Speed up Mission Control animations
-#defaults write com.apple.dock expose-animation-duration -float 0
-#
-## Don’t group windows by application in Mission Control
-#defaults write com.apple.dock expose-group-by-app -bool false
-#
-#defaults write com.apple.Dock autohide-delay -float 0.0001
-#
-## Automatically hide and show the Dock
-#defaults write com.apple.dock autohide -bool true
-#
-## Make Dock icons of hidden applications translucent
-#defaults write com.apple.dock showhidden -bool true
-#
+
+# Enable highlight hover effect for the grid view of a stack (Dock)
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
+
+# Dock icons size
+defaults write com.apple.dock tilesize -int 49
+
+# Change minimize/maximize window effect
+defaults write com.apple.dock mineffect -string "genie"
+
+# Minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -bool false
+
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Don’t animate opening applications from the Dock
+defaults write com.apple.dock launchanim -bool false
+
+# Speed up Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0
+
+# Don’t group windows by application in Mission Control
+defaults write com.apple.dock expose-group-by-app -bool false
+
+defaults write com.apple.Dock autohide-delay -float 0.0001
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+
 ################################################################################
 ## Terminal & iTerm 2                                                          #
 ################################################################################
-#
-## Only use UTF-8 in Terminal.app
-#defaults write com.apple.terminal StringEncodings -array 4
-#
+
+# Only use UTF-8 in Terminal.app
+defaults write com.apple.terminal StringEncodings -array 4
+
 ################################################################################
 ## Activity Monitor                                                            #
 ################################################################################
-#
-## Show the main window when launching Activity Monitor
-#defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
-#
-## Visualize CPU usage in the Activity Monitor Dock icon
-#defaults write com.apple.ActivityMonitor IconType -int 5
-#
-## Show all processes in Activity Monitor
-#defaults write com.apple.ActivityMonitor ShowCategory -int 0
-#
-## Sort Activity Monitor results by CPU usage
-#defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
-#defaults write com.apple.ActivityMonitor SortDirection -int 0
-#
-## Kill affected apps
-#for app in "Dock" "Finder"; do
-#  killall "${app}" >/dev/null 2>&1
-#done
+
+# Show the main window when launching Activity Monitor
+defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+
+# Visualize CPU usage in the Activity Monitor Dock icon
+defaults write com.apple.ActivityMonitor IconType -int 5
+
+# Show all processes in Activity Monitor
+defaults write com.apple.ActivityMonitor ShowCategory -int 100
+
+# Sort Activity Monitor results by CPU usage
+defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+defaults write com.apple.ActivityMonitor SortDirection -int 0
+
+# Kill affected apps
+for app in "Dock" "Finder"; do
+  killall "${app}" >/dev/null 2>&1
+done
