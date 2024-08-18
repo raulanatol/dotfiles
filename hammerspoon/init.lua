31fa4cc05258f4b5f85f0598afbece7765f39fe7
raulanatol <raul@natol.es>
2024-08-18 13:40:39 +0100

    Update hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,6 @@
-hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "E", function()
-    hs.execute("dot mac browser_goto", true)
-end)
+-- hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "E", function()
+--    hs.execute("dot mac browser_goto", true)
+-- end)
 
 hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "Z", function()
     hs.execute("dot mac zoom start", true)
