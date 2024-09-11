1dca2bb68b00ca530789e34176c2bac1e8d06ad3
raulanatol <raul@natol.es>
2024-09-11 08:35:08 +0100

    Checkpoint 11_09_2024

--- apps/wezterm/theme.lua
+++ apps/wezterm/theme.lua
@@ -42,7 +42,6 @@ function module.apply_to_config(config)
 
     config.use_fancy_tab_bar = false
     config.hide_tab_bar_if_only_one_tab = true
-    config.window_background_opacity = 0.9
 end
 
 return module
